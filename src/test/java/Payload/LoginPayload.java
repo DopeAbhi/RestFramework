654f40bb90b2db0e6703082cda5f0a2b3315cf6b
DopeAbhi <97584507+DopeAbhi@users.noreply.github.com>
2025-10-01 01:54:55 +0530

    Connection Invoice Payment Terms

--- src/test/java/Payload/LoginPayload.java
+++ src/test/java/Payload/LoginPayload.java
@@ -2,8 +2,7 @@ package Payload;
 
 public class LoginPayload {
 
-    public static String loginPayload()
-    {
+    public static String loginPayload() {
         return "{\n" +
                 "    \"companyCode\":\"MNDACNT220005\",\n" +
                 "    \"password\": \"NMiq+MkMjkUjW2xTtzMv2A==\",\n" +
@@ -11,91 +10,208 @@ public class LoginPayload {
                 "}";
     }
 
-    public static String redirectionUrlPayload(String accountStamp,String userStamp)
-    {
-        return "{\"accountStamp\": \""+accountStamp+"\",\n" +
+    public static String redirectionUrlPayload(String accountStamp, String userStamp) {
+        return "{\"accountStamp\": \"" + accountStamp + "\",\n" +
                 "\"dBKey\": \"1fMHyjzuikLVSV69c8E5jY77Fdgj0pXE9rNEson3NIw=\",\n" +
                 "\"productStamp\": \"c8d200cb-d2fb-4db8-9d5e-dcdaf2396a03\",\n" +
                 "\"subDomainUrl\": \"https://leasexuat.myndsolution.com/#/layout\",\n" +
-                "\"userStamp\": \""+userStamp+"\"}";
+                "\"userStamp\": \"" + userStamp + "\"}";
     }
 
 
-    public static String tokenByUser(String requestID)
-    {
+    public static String tokenByUser(String requestID) {
         return "{\n" +
-                "    \"stamp\":\""+requestID+"\"\n" +
+                "    \"stamp\":\"" + requestID + "\"\n" +
                 "}";
     }
 
 
-    public static String menuPayload(String type)
-    {
+    public static String menuPayload(String type) {
         return "{\n" +
-                "    \"type\":\""+type+"\"\n" +
+                "    \"type\":\"" + type + "\"\n" +
                 "    }";
     }
 
-    public static String recordListPayload(String tag,int page, int pageSize,String search)
-    {
+    public static String recordListPayload(String tag, int page, int pageSize, String search) {
         return "{\n" +
-                "    \"entity_tag\": \""+tag+"\",\n" +
-                "    \"page\":"+page+",\n" +
-                "    \"page_size\":"+pageSize+",\n" +
-                "    \"search\":\""+search+"\n" +
+                "    \"entity_tag\": \"" + tag + "\",\n" +
+                "    \"page\":" + page + ",\n" +
+                "    \"page_size\":" + pageSize + ",\n" +
+                "    \"search\":\"" + search + "\n" +
                 "}";
     }
 
-    public static String initiateTransitionPayload(String docType, String processName)
-    {
+    public static String initiateTransitionPayload(String docType, String processName) {
         return "{\n" +
                 "    \"action_id\": 0,\n" +
-                "    \"document_type\": \""+docType+"\",\n" +
+                "    \"document_type\": \"" + docType + "\",\n" +
                 "    \"instance_id\": 0,\n" +
                 "    \"meta_data\": {},\n" +
                 "    \"process_id\": 0,\n" +
-                "    \"process_name\": \""+processName+"\",\n" +
+                "    \"process_name\": \"" + processName + "\",\n" +
                 "    \"ref_id\": 0,\n" +
                 "    \"stage_id\": 0,\n" +
                 "    \"workspace_id\": 0\n" +
                 "}";
     }
 
-    public static String lessorBasicPayload(String email,String cin,String entityTag,String entityType,String erpCode,String lessorName)
-    {
+    public static String lessorBasicPayload(String email, String cin, String entityTag, String entityType, String erpCode, String lessorName) {
         return "{\n" +
-                "\"cin_number\": \""+cin+"\",\n" +
-                "\"email\": \""+email+"\",\n" +
+                "\"cin_number\": \"" + cin + "\",\n" +
+                "\"email\": \"" + email + "\",\n" +
                 "\"entity_stamp\": \"1aa002c83fb9b741a74cdd8dfe18d139\",\n" +
-                "\"entity_tag\": \""+entityTag+"\",\n" +
-                "\"entity_type\": \""+entityType+"\",\n" +
-                "\"erp_code\": \""+erpCode+"\",\n" +
-                "\"name\": \""+lessorName+"\",\n" +
+                "\"entity_tag\": \"" + entityTag + "\",\n" +
+                "\"entity_type\": \"" + entityType + "\",\n" +
+                "\"erp_code\": \"" + erpCode + "\",\n" +
+                "\"name\": \"" + lessorName + "\",\n" +
                 "\"vendor_code\": null\n" +
                 "}";
     }
 
-    public static String statePayload(String countryId,String stamp)
-    {
+    public static String statePayload(String countryId, String stamp) {
         return "{\n" +
-                "    \"id\": \""+countryId+"\",\n" +
+                "    \"id\": \"" + countryId + "\",\n" +
                 "    \"ifscCode\": \"\",\n" +
-                "    \"stamp\": \""+stamp+"\"\n" +
+                "    \"stamp\": \"" + stamp + "\"\n" +
                 "}";
     }
 
-    public static String cityPayload(String stateId,String stamp)
-    {
+    public static String cityPayload(String stateId, String stamp) {
         return "{\n" +
-                "    \"id\": \""+stateId+"\",\n" +
+                "    \"id\": \"" + stateId + "\",\n" +
                 "    \"ifscCode\": \"\",\n" +
-                "    \"stamp\": \""+stamp+"\"\n" +
+                "    \"stamp\": \"" + stamp + "\"\n" +
                 "}";
     }
 
-    public static String addressTypePayload(String adrsType)
+    public static String addressTypePayload(String adrsType) {
+        return "{\"type\":[\"" + adrsType + "\"]}";
+    }
+
+
+    public static String lessorAddressAddPayload(String adrsLine1, String adrsLine2
+            , String adrsType, String cityId, String countryId, String entityId, String stateId, String zipCode) {
+        return "{\"address_line1\": \"" + adrsLine1 + "\",\n" +
+                "\"address_line2\": \"" + adrsLine2 + "\",\n" +
+                "\"address_type\":\" " + adrsType + " \",\n" +
+                "\"city_id\":\" " + cityId + "\",\n" +
+                "\"country_id\":\" " + countryId + "\",\n" +
+                "\"entity_id\":\"" + entityId + "\",\n" +
+                "\"entity_tag\": \"lessor\",\n" +
+                "\"id\": null,\n" +
+                "\"state_id\": \"" + stateId + "\",\n" +
+                "\"zip_code\": \"" + zipCode + "\"}";
+    }
+
+    public static String lessorContactPersonPayload(String email,
+                                                    String entityId, String mobNum, String name) {
+        return "{\n" +
+                "    \"department_id\": null,\n" +
+                "    \"email_id\": \"" + email + "\",\n" +
+                "    \"entityTag\": \"lessor\",\n" +
+                "    \"entity_id\": \"" + entityId + "\",\n" +
+                "    \"is_primary\": false,\n" +
+                "    \"mobile_number1\": \"" + mobNum + "\",\n" +
+                "    \"name\": \"" + name + "\"\n" +
+                "}";
+    }
+
+    public static String compliancePayload(String entityId, String msmeExpreDte
+            , String msmeRegNo, String panNo) {
+        return "{\n" +
+                "    \"aadhaar_copy\": null,\n" +
+                "    \"aadhaar_no\": null,\n" +
+                "    \"declaration_letter\": {\n" +
+                "        \"docId\": 1716,\n" +
+                "        \"fileName\": \"http_status_methods_1744028189.pdf\"\n" +
+                "    },\n" +
+                "    \"entity_id\": " + entityId + ",\n" +
+                "    \"entity_stamp\": null,\n" +
+                "    \"entity_tag\": \"lessor\",\n" +
+                "    \"id\": null,\n" +
+                "    \"is_invoice_applied\": false,\n" +
+                "    \"msme_expiry_date\": \"" + msmeExpreDte + "\",\n" +
+                "    \"msme_register\": \"4\",\n" +
+                "    \"msme_register_no\": \"" + msmeRegNo + "\",\n" +
+                "    \"msme_registration_certificate\": null,\n" +
+                "    \"pan_copy\": {\n" +
+                "        \"docId\": 1715,\n" +
+                "        \"fileName\": \"ISTQB_CTFL_Syllabus-v4.0.pdf\"\n" +
+                "    },\n" +
+                "    \"pan_no\": \"" + panNo + "\"\n" +
+                "}";
+    }
+
+    public static String gstPayload(String entityId, String stateId, String gstNumber
+    ) {
+        return "{\n" +
+                "\"entity_id\": " + entityId + ",\n" +
+                "\"entity_stamp\": \"6c935e7f090f076c990b06a26ba1c11d\",\n" +
+                "\"entity_tag\": \"lessor\",\n" +
+                "\"gst_certificate\": null,\n" +
+                "\"gst_number\": \"" + gstNumber + "\",\n" +
+                "\"gst_status\": \"34\",\n" +
+                "\"state_id\": \"" + stateId + "\"\n" +
+                "}";
+    }
+
+    public static String accountPayoutType() {
+        return "{\n" +
+                "    \"type\": [\n" +
+                "        \"bank_account_type\",\n" +
+                "        \"bank_payout_type\"\n" +
+                "    ]\n" +
+                "}";
+    }
+
+    public static String lessorBankPayload(
+            String accHolderName, String accNo,
+            String accTypeId, String bankName, String payoutId,
+            String branchName, String entityId, String ifscCode
+    ) {
+        return "{\n" +
+                "\"account_holder_name\":\""+accHolderName+"\",\n" +
+                "\"account_no\": \""+accNo+"\",\n" +
+                "\"account_type_id\": \""+accTypeId+"\",\n" +
+                "\"bank_name\": \""+bankName+"\",\n" +
+                "\"bank_payout_type_id\": \""+payoutId+"\",\n" +
+                "\"branch_name\": \""+branchName+"\",\n" +
+                "\"cancelled_cheque\": null,\n" +
+                "\"entity_id\": "+entityId+",\n" +
+                "\"entity_tag\": \"lessor\",\n" +
+                "\"ifsc_code\": \""+ifscCode+"\"\n" +
+                "}";
+    }
+
+
+    public static String tdsPayload(String compId, String entityId, String taxCodeRate)
     {
-        return "{\"type\":[\""+adrsType+"\"]}";
+        return "{\n" +
+                "    \"component_id\": "+compId+",\n" +
+                "    \"entity_id\": "+entityId+",\n" +
+                "    \"tax_code_rate\": \""+taxCodeRate+"\"\n" +
+                "}";
+    }
+
+    public static String subCodePayload(String addressId, String bankId,
+                                        String contactId, String entityId,
+                                        String gstId, String paymentTermID, String siteCode, String siteName)
+    {
+        return "{\n" +
+                "\"address_id\": "+addressId+",\n" +
+                "\"bank_ids\": ["+bankId+"],\n" +
+                "\"company_id\": 1214,\n" +
+                "\"contact_person\": ["+contactId+"],\n" +
+                "\"entityId\": "+entityId+",\n" +
+                "\"entity_id\": "+entityId+",\n" +
+                "\"entity_stamp\": \"2688998cdf2511161229c2a65630a9c5\",\n" +
+                "\"entity_tag\": \"lessor\",\n" +
+                "\"erp_code\": null,\n" +
+                "\"gst_id\": "+gstId+",\n" +
+                "\"invoice_payment_terms_id\": "+paymentTermID+",\n" +
+                "\"site_code\": \""+siteCode+"\",\n" +
+                "\"site_name\": \""+siteName+"\"\n" +
+                "}";
     }
 
 }
--- src/test/java/TestFlow/LessorFlow/LessorRegistrationFlow.java
+++ src/test/java/TestFlow/LessorFlow/LessorRegistrationFlow.java
@@ -8,6 +8,7 @@ import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.specification.RequestLogSpecification;
 import io.restassured.specification.RequestSpecification;
+import net.datafaker.Faker;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -21,18 +22,31 @@ import static org.hamcrest.Matchers.matchesPattern;
 public class LessorRegistrationFlow {
 
 
-
     @Test
     public void lessorRegistrationFlow() throws IOException {
         String requestId;
         String authToken = null;
 
+        Faker faker = new Faker();
+        String email = faker.internet().emailAddress();
+        String lessorName = faker.name().fullName();
+        String stateName = "Delhi";
+        String cityName = "Delhi";
+        String addrsLine1 = faker.address().fullAddress();
+        String addrsLine2 = faker.address().fullAddress();
+        String zipCode = faker.address().zipCode();
+        String panNumber = "BYVPV091N";
+        String msmeNumber = "123456789012";
+        String gstNumber = "09AAACH7409R1ZZ";
+        String taxCodeTitle = "IN- Sec 194I-Invoice HUF-Rent Property 10%";
+        String tdsComponent="Minimum Guarantee";
+        String paymentTerm="7 Days";
 //==============================================Login API =========================================================
 
         RequestSpecification saasRequest = new RequestSpecBuilder().setBaseUri("https://myndsaasuat.myndsolution.com/")
                 .setContentType(ContentType.JSON).build();
 
-        RequestSpecification workflowRequest = new RequestSpecBuilder().setBaseUri("https://uatworkflow.myndsolution.com").setContentType(ContentType.JSON).build();
+        RequestSpecification workflowRequest = new RequestSpecBuilder().setBaseUri("https://uatworkflow.myndsolution.com/").setContentType(ContentType.JSON).build();
         RequestSpecification vendorRequest = new RequestSpecBuilder().setBaseUri("https://uatvendor.myndsolution.com/api/").setContentType(ContentType.JSON).build();
 
 
@@ -76,70 +90,182 @@ public class LessorRegistrationFlow {
 //==============================================Menu========================================================
 
         given().spec(workflowRequest).header("Authorization", "Bearer " + authToken).body(LoginPayload.menuPayload("left"))
-                .when().post("/api/menu").then()
+                .when().post("api/menu").then()
                 .log().all();
 
 //==============================================Record List========================================================
-        given().spec(vendorRequest).header("Authorization","Bearer "+authToken).body(LoginPayload.recordListPayload("lessor",1,10,""))
+        given().spec(vendorRequest).header("Authorization", "Bearer " + authToken).body(LoginPayload.recordListPayload("lessor", 1, 10, ""))
                 .when().post("vendor/list").then()
                 .log().all();
 
 //==============================================Initiate Transition========================================================
-        given().spec(workflowRequest).header("Authorization", "Bearer "+authToken).body(LoginPayload.initiateTransitionPayload("lessor","lessor_registration"))
-                .when().post("/api/internal/wf/initiate_transition").then()
+        given().spec(workflowRequest).header("Authorization", "Bearer " + authToken).body(LoginPayload.initiateTransitionPayload("lessor", "lessor_registration"))
+                .when().post("api/internal/wf/initiate_transition").then()
                 .log().all();
 
 //==============================================Lessor Basic Add===========================================================
-     String basicRespone=   given().spec(vendorRequest).header("Authorization","Bearer "+authToken)
-                .body(LoginPayload.lessorBasicPayload("AutomateAPI@gmail.com","Test","lessor","1","Test","AutomateAPI"))
+        String basicRespone = given().spec(vendorRequest).header("Authorization", "Bearer " + authToken)
+                .body(LoginPayload.lessorBasicPayload(email, "Test", "lessor", "1", "Test", lessorName))
                 .when().post("register/add").then()
                 .log().all().extract().response().asString();
-          js=  CommonUtility.rawtoJson(basicRespone);
+        js = CommonUtility.rawtoJson(basicRespone);
 
-        String lessorId=  js.getString("data.id");
+        String entityId = js.getString("data.id");
 
 //==============================================Get Countries API===========================================================
-      String countryResp=  given().spec(saasRequest).header("Authorization", "Bearer "+authToken)
+        String countryResp = given().spec(saasRequest).header("Authorization", "Bearer " + authToken)
                 .body(" ")
                 .when().post("mst/api/Country/GetCountries")
                 .then().log().all().extract().response().asString();
 
-                js=CommonUtility.rawtoJson(countryResp);
-                String countryId=js.getString("data.countryId");
+        js = CommonUtility.rawtoJson(countryResp);
+        String countryId = js.getString("data[0].countryId");
 
 //==============================================Get State by Country===========================================================
 
-       String stateResp= given().spec(saasRequest).header("Authorization","Bearer "+authToken)
-                .body(LoginPayload.statePayload(countryId,requestId))
+        String stateResp = given().spec(saasRequest).header("Authorization", "Bearer " + authToken)
+                .body(LoginPayload.statePayload(countryId, requestId))
                 .when().post("mst/api/State/GetStatesByCountryId").then()
                 .log().all().extract().response().asString();
-
-       js=CommonUtility.rawtoJson(stateResp);
-       String stateId=js.getString("data.stateId");
+        String stateId = null;
+        js = CommonUtility.rawtoJson(stateResp);
+        for (int i = 0; i < js.getInt("data.size()"); i++) {
+            if (js.getString("data[" + i + "].name").equalsIgnoreCase(stateName)) {
+                stateId = js.getString("data[" + i + "].stateId");
+                break;
+            }
+        }
 //==============================================Get City by State===========================================================
 
-      String cityResp=  given().spec(saasRequest).header("Authorization","Bearer "+authToken)
-                .body(LoginPayload.cityPayload(stateId,requestId))
+        String cityResp = given().spec(saasRequest).header("Authorization", "Bearer " + authToken)
+                .body(LoginPayload.cityPayload(stateId, requestId))
                 .when().post("mst/api/City/GetCityByStateId")
                 .then().log().all().extract().response().asString();
 
-      js=CommonUtility.rawtoJson(cityResp);
-     String cityId= js.getString("data.cityId");
+        js = CommonUtility.rawtoJson(cityResp);
+        String cityId = null;
+        for (int i = 0; i < js.getInt("data.size()"); i++) {
+            if (js.getString("data[" + i + "].name").equalsIgnoreCase(cityName)) {
+                cityId = js.getString("data[" + i + "].cityId");
+                break;
+            }
+        }
 //==============================================Get AddressType===========================================================
-        String addressId=null;
-       String addrssTypeResp= given().spec(vendorRequest).header("Authorization" ,"Bearer "+authToken)
+        String addressId = null;
+        String addrssTypeResp = given().spec(vendorRequest).header("Authorization", "Bearer " + authToken)
                 .body(LoginPayload.addressTypePayload("address_type"))
                 .when().post("master/lookup")
                 .then().log().all().extract().response().asString();
-       js=CommonUtility.rawtoJson(addrssTypeResp);
-      int count=  js.getInt("data.address_type");
-        for (int i = 0; i <count ; i++) {
-            if(js.getString("data.address_type["+i+"].name").equalsIgnoreCase("Corporate"))
-            {
-                addressId=js.getString("data.address_type["+i+"].id");
+        js = CommonUtility.rawtoJson(addrssTypeResp);
+
+        for (int i = 0; i < js.getInt("data.address_type"); i++) {
+            if (js.getString("data.address_type[" + i + "].name").equalsIgnoreCase("Corporate")) {
+                addressId = js.getString("data.address_type[" + i + "].id");
+                break;
             }
         }
 //==============================================Lessor Address Add API===========================================================
+
+
+        String addrsResp = given().spec(vendorRequest).header("Authorization", "Bearer " + authToken)
+                .body(LoginPayload.lessorAddressAddPayload(addrsLine1, addrsLine2, addressId
+                        , cityId, countryId, entityId, stateId, zipCode))
+                .when().post("api/address/add")
+                .then().log().all().extract().response().asString();
+        js = CommonUtility.rawtoJson(addrsResp);
+        int addrsId = js.getInt("data.id");
+//===============================Lessor Contact Person Add API =================================================
+        String contactResp = given().spec(vendorRequest).header("Authorization", "Bearer " + authToken)
+                .body(LoginPayload.lessorContactPersonPayload(email, entityId, faker.phoneNumber().cellPhone()
+                        , lessorName)).when().post()
+                .then().log().all().extract().response().asString();
+        js = CommonUtility.rawtoJson(contactResp);
+        int contactId = js.getInt("data.id");
+//===============================Lessor Compliance Details =================================================
+
+        given().spec(vendorRequest).header("Authorization", "Bearer " + authToken)
+                .body(LoginPayload.compliancePayload(entityId, "2090-11-09", msmeNumber,
+                        panNumber)).when().post("compliance/pan_add")
+                .then().log().all().extract().response().asString();
+//===============================Lessor GST Add API =================================================
+
+        String gstResp = given().spec(vendorRequest).header("Authorization", "Bearer " + authToken)
+                .body(LoginPayload.gstPayload(entityId, stateId, gstNumber)
+                ).when().post("compliance/gst_add")
+                .then().log().all().extract().response().asString();
+        js = CommonUtility.rawtoJson(gstResp);
+        int gstId = js.getInt("data.id");
+//===============================Account Payout Type API =================================================
+
+        String accPayResp = given().spec(vendorRequest).header("Authorization", "Bearer " + authToken)
+                .body(LoginPayload.accountPayoutType())
+                .when().post("/master/lookup")
+                .then().log().all().extract().response().asString();
+        js = CommonUtility.rawtoJson(accPayResp);
+        String payoutTypeId = js.getString("data.bank_account_type[0].id");
+        String accType = js.getString("data.bank_account_type[0].name");
+//===============================Bank Details Add API =================================================
+        String bankResp = given().spec(vendorRequest).header("Authorization", "Bearer " + authToken)
+                .body(LoginPayload.lessorBankPayload("Test Holder"
+                        , "1231231233", accType, "Test Bank", payoutTypeId, "Test Branch", entityId,
+                        "HDFC0001234"))
+                .when().post("bank/add").then()
+                .log().all().extract().response().asString();
+        js = CommonUtility.rawtoJson(bankResp);
+        int bankId = js.getInt("data.id");
+//===============================Get Tax Code API =================================================
+        String taxCodeRate = null;
+        String taxCodeResp = given().spec(vendorRequest).header("Authorization", "Bearer " + authToken)
+                .body("")
+                .when().post("vendor/tax_code_rate").then()
+                .log().all().extract().response().asString();
+        js = CommonUtility.rawtoJson(taxCodeResp);
+        for (int i = 0; i < js.getInt("data.size()"); i++) {
+            if (js.getString("data[" + i + "].display_title").equalsIgnoreCase(taxCodeTitle)) {
+                taxCodeRate = js.getString("data[" + i + "].tax_code_rate");
+                break;
+            }
+        }
+//===============================Get TDS Component API =================================================
+      String taxComponentId = null;
+        String tdsCompResp=given().spec(vendorRequest).header("Authorization", "Bearer " + authToken)
+                .body("")
+                .when().post("vendor/component_list").then()
+                .log().all().extract().response().asString();
+        for (int i = 0; i < js.getInt("data.size()"); i++) {
+            if (js.getString("data[" + i + "].name").equalsIgnoreCase(tdsComponent)) {
+                taxComponentId = js.getString("data[" + i + "].id");
+                break;
+            }
+        }
+
+        //===============================Add TDS API =================================================
+        given().spec(vendorRequest).header("Authorization", "Bearer " + authToken)
+                .body(LoginPayload.tdsPayload(taxComponentId, entityId, taxCodeRate))
+                .when().post("tds/add")
+                .then().log().all().extract().response().asString();
+        //===============================Get Invoice Payment Terms =================================================
+
+       String payTermsResp= given().spec(vendorRequest).header("Authorization", "Bearer " + authToken)
+                .body("{\n" +
+                        "    \"type\":[\n" +
+                        "        \"invoice_payment_terms\"\n" +
+                        "    ]\n" +
+                        "}")
+                .when().post("master/lookup")
+                .then().log().all().extract().response().asString();
+            js=CommonUtility.rawtoJson(payTermsResp);
+        for (int i = 0; i <js.getInt("data.invoice_payment_terms.size()") ; i++) {
+            if(js.getString("data.invoice_payment_terms["+i+"].name").equalsIgnoreCase("Net 30")){
+                String paymentTermID=js.getString("data.invoice_payment_terms["+i+"].id");
+                break;
+            }
+        //==============================Lessor SubCode Add=================================================
+
+        given().spec(vendorRequest).header("Authorization", "Bearer " + authToken)
+                .body(LoginPayload.subCodePayload())
+                .when().post("subcode/add")
+                .then().log().all().extract().response().asString();
 
     }
 }
